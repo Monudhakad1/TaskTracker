323c23e64d87be2940a3a483a85b2953c5fc98a1
Monudhakad1 <monudhakad055@gmail.com>
2025-12-07 23:19:41 +0530

    Ad project

---
+++ src/main/java/com/tracker/newtracker/NewTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.newtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tracker/newtracker/NewTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.newtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
